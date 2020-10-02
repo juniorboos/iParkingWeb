0740894abadb2abe478523eda32251b31ab3fa21
Milton Boos Junior <junior_boos@live.com>
2020-10-02 05:52:39 +0100

    cards finished

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './styles.css';
 
 import logo from './assets/logo.svg';
 import iphoneHome from './assets/iphone1.png';
+import card1 from './assets/card1.svg';
+import card2 from './assets/card2.svg';
+import card3 from './assets/card3.svg';
+import sectionTitle from './assets/sectionTitle.svg';
 
 function App() {
   return (
@@ -16,11 +20,39 @@ function App() {
         </div>
       </div>
       <div className="home">
-        <img className="iphoneHome" src={iphoneHome} alt="Iphone homepage"/>
-        <h1 className="homeTitle">a solution for Smart Parkings</h1>
-      </div>
-      <div className="about">
+        <div className="homeItems">
+          <img className="iphoneHome" src={iphoneHome} alt="Iphone homepage"/>
+          <h1 className="homeTitle">a solution for Smart Parkings</h1>
+        </div>
+        
 
+      </div>
+      <div className="cards">
+          <div className="card">
+            <img className="cardImage" src={card1} alt="card 1 image"/>
+            <div>
+              <h3 className="cardTitle">Multi-agent system</h3>
+              <p className="cardDescription">Best spot for your needs</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="cardImage" src={card2} alt="card 1 image"/>
+            <div>
+              <h3 className="cardTitle">Cloud based</h3>
+              <p className="cardDescription">Fast and efficient</p>
+            </div>
+          </div>
+          <div className="card">
+            <img className="cardImage" src={card3} alt="card 1 image"/>
+            <div>
+              <h3 className="cardTitle">Easy to use</h3>
+              <p className="cardDescription">Very intuitive</p>
+            </div>
+          </div>
+        </div>
+      <div className="about">
+        <h2 className="sectionTitle">About App</h2>
+        <img src={sectionTitle} alt="section svg"/>
       </div>
     </div>
   );
