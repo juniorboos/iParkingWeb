40712cf1b2b33e81a64e17f423e8a8b1a7334674
Milton Boos Junior <junior_boos@live.com>
2020-10-02 06:14:51 +0100

    about section added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import card1 from './assets/card1.svg';
 import card2 from './assets/card2.svg';
 import card3 from './assets/card3.svg';
 import sectionTitle from './assets/sectionTitle.svg';
+import aboutScreens from './assets/aboutScreens.png';
 
 function App() {
   return (
@@ -28,32 +29,48 @@ function App() {
 
       </div>
       <div className="cards">
-          <div className="card">
-            <img className="cardImage" src={card1} alt="card 1 image"/>
-            <div>
-              <h3 className="cardTitle">Multi-agent system</h3>
-              <p className="cardDescription">Best spot for your needs</p>
-            </div>
-          </div>
-          <div className="card">
-            <img className="cardImage" src={card2} alt="card 1 image"/>
-            <div>
-              <h3 className="cardTitle">Cloud based</h3>
-              <p className="cardDescription">Fast and efficient</p>
-            </div>
-          </div>
-          <div className="card">
-            <img className="cardImage" src={card3} alt="card 1 image"/>
-            <div>
-              <h3 className="cardTitle">Easy to use</h3>
-              <p className="cardDescription">Very intuitive</p>
-            </div>
+        <div className="card">
+          <img className="cardImage" src={card1} alt="card 1 image"/>
+          <div>
+            <h3 className="cardTitle">Multi-agent system</h3>
+            <p className="cardDescription">Best spot for your needs</p>
           </div>
         </div>
+        <div className="card">
+          <img className="cardImage" src={card2} alt="card 1 image"/>
+          <div>
+            <h3 className="cardTitle">Cloud based</h3>
+            <p className="cardDescription">Fast and efficient</p>
+          </div>
+        </div>
+        <div className="card">
+          <img className="cardImage" src={card3} alt="card 1 image"/>
+          <div>
+            <h3 className="cardTitle">Easy to use</h3>
+            <p className="cardDescription">Very intuitive</p>
+          </div>
+        </div>
+      </div>
+      <div className="sectionHeader">
+        <h2 className="sectionTitle">About App</h2>
+        <img className="sectionSvg" src={sectionTitle} alt="section svg"/>
+      </div>
       <div className="about">
-        <div className="sectionHeader">
-          <h2 className="sectionTitle">About App</h2>
-          <img className="sectionSvg" src={sectionTitle} alt="section svg"/>
+        <img className="aboutScreens" src={aboutScreens} alt="iphone screens"/>
+        <div className="aboutText">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiuiosmod terttmpor incididunt
+            ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. doliuor in reprehenderit in voluptate velit
+            esse dolore eu fugiat nulla pariatur. cdatat non proident
+          </p>
+          <br/>
+          <br/>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tuiempor incididunt 
+            ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip
+          </p>
         </div>
 
       </div>
