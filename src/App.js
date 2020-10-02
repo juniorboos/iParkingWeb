89264b63103c89716298e3d6901e25964e2bad41
Milton Boos Junior <junior_boos@live.com>
2020-10-02 05:08:12 +0100

    home page stylized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './styles.css';
 
+import logo from './assets/logo.svg';
+import iphoneHome from './assets/iphone1.png';
+
 function App() {
   return (
-    <div>
-        
+    <div className="mainContainer">
+      <div className="header">
+        <img className="logo" src={logo} alt="iParking logo"/>
+        <div className="headerItems">
+          <h4 className="headerItem">About</h4>
+          <h4 className="headerItem">Features</h4>
+          <h4 className="headerItem">Download</h4>
+        </div>
+      </div>
+      <div className="home">
+        <img className="iphoneHome" src={iphoneHome} alt="Iphone homepage"/>
+        <h1 className="homeTitle">a solution for Smart Parkings</h1>
+      </div>
+      <div className="about">
+
+      </div>
     </div>
   );
 }
